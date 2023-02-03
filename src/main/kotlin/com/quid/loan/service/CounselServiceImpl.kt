45be9d83cc7e476f3d6ef29048c47081ce3d1f19
QuiD-0 <wodnd101@naver.com>
2023-02-03 14:12:28 +0900

    Kotlin Logging을 추가하였습니다.

--- src/main/kotlin/com/quid/loan/service/CounselServiceImpl.kt
+++ src/main/kotlin/com/quid/loan/service/CounselServiceImpl.kt
@@ -9,32 +9,39 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CounselServiceImpl(private val counselRepository: CounselRepository) : CounselService {
+    private val logger = mu.KotlinLogging.logger {}
+
     @Transactional
     override
     fun createCounsel(counselRequest: CounselRequest): CounselResponse {
+        logger.info { "createCounsel: $counselRequest" }
         val counsel = counselRepository.createCounsel(Counsel.of(counselRequest))
         return CounselResponse.of(counsel)
     }
 
     override
     fun getCounsel(counselId: Long): Counsel {
+        logger.info { "getCounsel: $counselId" }
         return counselRepository.findById(counselId)
     }
 
     @Transactional
     override
     fun updateCounselMemo(counselId: Long, memo: String) {
+        logger.info { "updateCounselMemo: $counselId, $memo" }
         counselRepository.updateMemo(counselId, memo)
     }
 
     @Transactional
     override
     fun deleteCounsel(counselId: Long) {
+        logger.info { "deleteCounsel: $counselId" }
         counselRepository.deleteById(counselId)
     }
 
     override
     fun getCounsels(): List<Counsel> {
+        logger.info { "getCounsels" }
         return counselRepository.getCounsels()
     }
 }
