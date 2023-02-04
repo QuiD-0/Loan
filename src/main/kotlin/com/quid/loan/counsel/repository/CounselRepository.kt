9311e2e72b9c20691bbe2f96d3c9ca664a91f63b
quid-0 <wodnd101@naver.com>
2023-02-04 15:24:09 +0900

    counsel 서비스와 레파지토리의 위치를 이동하였습니다.

--- src/main/kotlin/com/quid/loan/counsel/repository/CounselRepository.kt
+++ src/main/kotlin/com/quid/loan/counsel/repository/CounselRepository.kt
@@ -1,8 +1,12 @@
 package com.quid.loan.counsel.repository
 
 import com.quid.loan.counsel.domain.Counsel
+import com.quid.loan.utils.StatusCode
+import com.quid.loan.utils.fail
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
 
 interface CounselRepository {
     fun createCounsel(counsel: Counsel): Counsel
@@ -11,4 +15,35 @@ interface CounselRepository {
     fun deleteById(counselId: Long)
     fun updateMemo(counselId: Long, memo: String)
     fun isExistCounsel(name: String): Boolean
+
+
+    @Repository
+    class CounselRepositoryImpl(private val counselJpaRepository: CounselJpaRepository) :
+        CounselRepository {
+        override fun createCounsel(counsel: Counsel): Counsel {
+            return counselJpaRepository.save(counsel)
+        }
+
+        override fun findById(counselId: Long): Counsel {
+            return counselJpaRepository.findByIdOrNull(counselId) ?: fail(StatusCode.COUNSEL_NOT_FOUND_ERROR)
+        }
+
+        override fun getCounsels(pageable : Pageable): Page<Counsel> {
+            return counselJpaRepository.findAll(pageable)
+        }
+
+        override fun deleteById(counselId: Long) {
+            return counselJpaRepository.deleteById(counselId)
+        }
+
+        override fun updateMemo(counselId: Long, memo: String) {
+            counselJpaRepository.findByIdOrNull(counselId)?.updateMemo(memo)
+                ?: fail(StatusCode.COUNSEL_NOT_FOUND_ERROR)
+        }
+
+        override fun isExistCounsel(name: String): Boolean {
+            return counselJpaRepository.existsByName(name)
+        }
+
+    }
 }
