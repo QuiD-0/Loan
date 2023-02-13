188f4d7c01a99ccecbc94ba67611e6f72b3f0f22
QuiD-0 <wodnd101@naver.com>
2023-02-13 17:44:48 +0900

    대출 생성의 위치를 수정하였습니다.

--- src/main/kotlin/com/quid/loan/loan/service/LoanService.kt
+++ src/main/kotlin/com/quid/loan/loan/service/LoanService.kt
@@ -1,8 +1,6 @@
 package com.quid.loan.loan.service
 
-import com.quid.loan.loan.domain.Loan
 import com.quid.loan.loan.dto.LoanCreateRequest
-import com.quid.loan.loan.repository.LoanRepository
 import com.quid.loan.user.repository.UserRepository
 import com.quid.loan.utils.StatusCode
 import com.quid.loan.utils.fail
@@ -14,7 +12,6 @@ interface LoanService {
 
     @Service
     class LoanServiceImp(
-        private val loanRepository: LoanRepository,
         private val userRepository: UserRepository
     ) : LoanService {
 
@@ -22,8 +19,7 @@ interface LoanService {
         override fun createLoan(userSeq: Long, request: LoanCreateRequest) {
             val user = userRepository.findById(userSeq)
             if (user.counsel == null) fail(StatusCode.COUNSEL_NOT_FOUND_ERROR)
-            val loan = Loan.create(request, user)
-            user.loan = loan
+            user.createLoan(request)
         }
     }
 }
